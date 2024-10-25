f7f7c046b0293b7fd373d09e3e9150c7c328ba43
chohee.hwang <manent556@gmail.com>
2024-10-25 09:32:59 +0900

    파일이동

--- react/24.10.24/my-mood/src/App.js
+++ react/24.10.24/my-mood/src/App.js
@@ -1,8 +1,12 @@
-function App() {
+import React from "react";
+import BtnList from "./Components/BtnList";
+import "./App.css";
+
+export default function App() {
   return (
-    <div>
-      hello world
+    <div className="App">
+      <h1>오늘의 기분을 선택해주세요😄</h1>
+      <BtnList />
     </div>
   );
 }
-export default App;
